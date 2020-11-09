fbcd8288554c76ac5707f4fa1c9f1b2394cfb178
rcbb <rcbb@outlook.com>
2020-11-09 22:55:22 +0800

    0.0.10

--- test.py
+++ test.py
@@ -14,9 +14,28 @@ def on_error(ws, error):
 def on_close(ws):
     print("### closed ###")
 
-websocket.enableTrace(True)
-ws = websocket.WebSocketApp("wss://stream.binance.com:9443/ws/!ticker@arr",
-                          on_message = on_message,
-                          on_error = on_error,
-                          on_close = on_close)
-ws.run_forever(sslopt={"check_hostname": False})
+#开始连接时执行，需要订阅的消息和其它操作都要在这里完成
+def on_open(ws):
+    websocket.enableTrace(True)
+    ws = websocket.create_connection("wss://fstream.binance.com")
+    # print("Sending 'Hello, World'...")
+    # ws.send("Hello, World")
+    # print("Sent")
+    # print("Receiving...")
+    result = ws.recv()
+    print("Received '%s'" % result)
+    ws.close()
+    # ws.send(wsGetAccount('ok_sub_spotcny_userinfo',api_key,secret_key))
+    # ws.send("{'event':'addChannel','channel':'ok_sub_spotcny_btc_depth_60'}")
+    # ws.send("{'event':'addChannel','channel':'ok_sub_spotcny_btc_ticker'}")
+
+# websocket.enableTrace(True)
+# ws = websocket.WebSocketApp("wss://stream.binance.com:9443/ws/!ticker@arr",
+#                           on_message = on_message,
+#                           on_error = on_error,
+#                           on_close = on_close)
+# ws.on_open = on_open
+# ws.run_forever(sslopt={"check_hostname": False})
+
+
+on_open("")
